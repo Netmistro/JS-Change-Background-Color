5a37e715acb48de987ac7f347f84152d8ea2a067
Arnold Bradshaw <arnoldbradshaw@gmail.com>
2020-10-05 16:02:49 -0400

    Final Version

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
+// Setup constants
 const colorBtn = document.querySelector('.colorBtn');
 const bodyBcg = document.querySelector('body');
+const colors = ['yellow', 'green', 'blue', 'orange', 'purple', '#3b5998'];
+
+// Add event listener on click event
+colorBtn.addEventListener('click', changeColor);
+
+// Function to generate random colors
+function changeColor() {
+    let randomColor = Math.floor(Math.random() * colors.length);
+    bodyBcg.style.backgroundColor = colors[randomColor];
+}
